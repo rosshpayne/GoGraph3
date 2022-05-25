be2f310de6667ddf233fc5654b80e650d52a2718
rosshpayne <rosshpayne@gmail.com>
2022-05-25 22:34:38 +0000

    rename files

---
+++ db/reg-spanner.go
@@ -0,0 +1,83 @@
+//go:build spanner
+// +build spanner
+
+package db
+
+import (
+	"context"
+	"sync"
+
+	"github.com/GoGraph/tx/mut"
+	"github.com/GoGraph/tx/query"
+
+	"cloud.google.com/go/spanner"
+)
+
+type SpannerHandle struct {
+	opt Options
+	*spanner.Client
+}
+
+var (
+	dbSrv *spanner.Client
+	mu    sync.Mutex
+	// zero entry in dbRegistry is for default db.
+	// non-default db's use Register()
+	dbRegistry []RegistryT = []RegistryT{RegistryT{Name: "Spanner", Default: true}}
+)
+
+func newService() (*spanner.Client, error) {
+	// Note: NewClient does not error if instance is not available. Error is generated when db is accessed in type.graphShortName
+	ctx := context.Background()
+	client, err := spanner.NewClient(ctx, "projects/banded-charmer-310203/instances/test-instance/databases/test-sdk-db")
+	if err != nil {
+		logerr(err)
+		return nil, err
+	}
+	return client, nil
+}
+
+func init() {
+
+	var err error
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	dbSrv, err = newService()
+	if err != nil {
+		logerr(err)
+	}
+	dbRegistry[DefaultDB].Handle = SpannerHandle{Client: dbSrv}
+
+	// define dbSrv used by db package (dynamodb specific) internals - not ideal solution, would rather
+	// source from dbRegistry but this could be expensive at runtime. This works only because
+	// we are dealing with the default database, otherwise we would be forced to go through dbRegistry.
+}
+
+// Execute dml (see ExecuteQuery). TODO: make prepare a db.Option
+func (h SpannerHandle) Execute(ctx context.Context, bs []*mut.Mutations, tag string, api API, prepare bool, opt ...Option) error {
+
+	return execute(ctx, h.Client, bs, tag, api, opt...)
+}
+
+func (h SpannerHandle) ExecuteQuery(qh *query.QueryHandle, o ...Option) error {
+
+	return executeQuery(qh, o...)
+}
+
+func (h SpannerHandle) Close(q *query.QueryHandle) error {
+
+	return nil
+}
+
+func (h SpannerHandle) CloseTx(m []*mut.Mutations) {}
+
+func (h SpannerHandle) String() string {
+	if dbRegistry[0].Default {
+		return dbRegistry[0].Name + " [default]"
+	} else {
+		return dbRegistry[0].Name + " [not default]"
+	}
+
+}
